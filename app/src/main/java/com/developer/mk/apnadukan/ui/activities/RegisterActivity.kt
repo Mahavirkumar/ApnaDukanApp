e26c1f20d58b66e3d4d4ba1477437f8f0d27690a
Mahavir <kumar.mahavir@indiamart.com>
2022-07-24 09:57:33 +0530

    login,sign up implemted,with firebase sotre,but get issue in login with email,pasword

--- app/src/main/java/com/developer/mk/apnadukan/ui/activities/RegisterActivity.kt
+++ app/src/main/java/com/developer/mk/apnadukan/ui/activities/RegisterActivity.kt
@@ -4,43 +4,49 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.WindowManager
-import androidx.databinding.DataBindingUtil
+import android.widget.Toast
 import com.developer.mk.apnadukan.R
-import com.developer.mk.apnadukan.databinding.ActivityLoginBinding
-import com.developer.mk.apnadukan.databinding.ActivityRegisterBinding
+import com.developer.mk.apnadukan.firestore.FirestoreClass
+import com.developer.mk.apnadukan.models.User
+
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_register.*
 
 @Suppress("DEPRECATION")
 class RegisterActivity : BaseActivity() {
-    private lateinit var registerBinding: ActivityRegisterBinding
+    /**
+     * This function is auto created by Android when the Activity Class is created.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
+        //This call the parent constructor
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_register)
-        registerBinding = DataBindingUtil.setContentView(this, R.layout.activity_register)
+        // This is used to align the xml view to this class
+        setContentView(R.layout.activity_register)
 
+        // This is used to hide the status bar and make the splash screen as a full screen activity.
+        // It is deprecated in the API level 30. I will update you with the alternate solution soon.
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
+
         setupActionBar()
-        // END
 
-        // TODO Step 8: Assign a click event to the register button and call the validate function.
-        // START
-        registerBinding.btnRegister.setOnClickListener {
+        btn_register.setOnClickListener {
 
-            validateRegisterDetails()
+            registerUser()
         }
-        // END
     }
 
-    // TODO Step 1: Create a function to set up an action bar.
-    // START
     /**
      * A function for actionBar Setup.
      */
     private fun setupActionBar() {
 
-        setSupportActionBar(registerBinding.toolbarRegisterActivity)
+        setSupportActionBar(toolbar_register_activity)
 
         val actionBar = supportActionBar
         if (actionBar != null) {
@@ -48,57 +54,133 @@ class RegisterActivity : BaseActivity() {
             actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24dp)
         }
 
-        registerBinding.toolbarRegisterActivity.setNavigationOnClickListener { onBackPressed() }
+        toolbar_register_activity.setNavigationOnClickListener { onBackPressed() }
     }
-    // END
 
-    // TODO Step 6: Create an function to validate the register account fields.
-    // START
     /**
      * A function to validate the entries of a new user.
      */
     private fun validateRegisterDetails(): Boolean {
         return when {
-            TextUtils.isEmpty(registerBinding.etFirstName.text.toString().trim { it <= ' ' }) -> {
+            TextUtils.isEmpty(et_first_name.text.toString().trim { it <= ' ' }) -> {
                 showErrorSnackBar(resources.getString(R.string.err_msg_enter_first_name), true)
                 false
             }
 
-            TextUtils.isEmpty(registerBinding.etLastName.text.toString().trim { it <= ' ' }) -> {
+            TextUtils.isEmpty(et_last_name.text.toString().trim { it <= ' ' }) -> {
                 showErrorSnackBar(resources.getString(R.string.err_msg_enter_last_name), true)
                 false
             }
 
-            TextUtils.isEmpty(registerBinding.etEmail.text.toString().trim { it <= ' ' }) -> {
+            TextUtils.isEmpty(et_email.text.toString().trim { it <= ' ' }) -> {
                 showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), true)
                 false
             }
 
-            TextUtils.isEmpty(registerBinding.etPassword.text.toString().trim { it <= ' ' }) -> {
+            TextUtils.isEmpty(et_password.text.toString().trim { it <= ' ' }) -> {
                 showErrorSnackBar(resources.getString(R.string.err_msg_enter_password), true)
                 false
             }
 
-            TextUtils.isEmpty(registerBinding.etConfirmPassword.text.toString().trim { it <= ' ' }) -> {
-                showErrorSnackBar(resources.getString(R.string.err_msg_enter_confirm_password), true)
+            TextUtils.isEmpty(et_confirm_password.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_enter_confirm_password),
+                    true
+                )
                 false
             }
 
-            registerBinding.etPassword.text.toString().trim { it <= ' ' }
-                    != registerBinding.etConfirmPassword.text.toString()
+            et_password.text.toString().trim { it <= ' ' } != et_confirm_password.text.toString()
                 .trim { it <= ' ' } -> {
-                showErrorSnackBar(resources.getString(R.string.err_msg_password_and_confirm_password_mismatch), true)
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_password_and_confirm_password_mismatch),
+                    true
+                )
                 false
             }
-            !registerBinding.cbTermsAndCondition.isChecked -> {
-                showErrorSnackBar(resources.getString(R.string.err_msg_agree_terms_and_condition), true)
+            !cb_terms_and_condition.isChecked -> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_agree_terms_and_condition),
+                    true
+                )
                 false
             }
             else -> {
-                showErrorSnackBar("Your details are valid.", false)
                 true
             }
         }
     }
 
+    /**
+     * A function to register the user with email and password using FirebaseAuth.
+     */
+    private fun registerUser() {
+
+        // Check with validate function if the entries are valid or not.
+        if (validateRegisterDetails()) {
+
+            // Show the progress dialog.
+            showProgressDialog(resources.getString(R.string.please_wait))
+
+            val email: String = et_email.text.toString().trim { it <= ' ' }
+            val password: String = et_email.text.toString().trim { it <= ' ' }
+
+            // Create an instance and create a register a user with email and password.
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(
+                    OnCompleteListener<AuthResult> { task ->
+
+                        // If the registration is successfully done
+                        if (task.isSuccessful) {
+
+                            // Firebase registered user
+                            val firebaseUser: FirebaseUser = task.result!!.user!!
+
+                            // Here we have passed only four values in the constructor as there are only four values at registration. So, instead of giving it blank or default.
+                            // We have already added the default values in the data model class itself. Make sure the passing value order is correct.
+
+                            // Instance of User data model class.
+                            val user = User(
+                                firebaseUser.uid,
+                                et_first_name.text.toString().trim { it <= ' ' },
+                                et_last_name.text.toString().trim { it <= ' ' },
+                                et_email.text.toString().trim { it <= ' ' }
+                            )
+
+                            // Pass the required values in the constructor.
+                            FirestoreClass().registerUser(this@RegisterActivity, user)
+
+                        } else {
+                            // Hide the progress dialog
+                            hideProgressDialog()
+                            // If the registering is not successful then show error message.
+                            showErrorSnackBar(task.exception!!.message.toString(), true)
+                        }
+                    })
+        }
+    }
+
+    /**
+     * A function to notify the success result of Firestore entry when the user is registered successfully.
+     */
+    fun userRegistrationSuccess() {
+
+        // Hide the progress dialog
+        hideProgressDialog()
+
+        Toast.makeText(
+            this@RegisterActivity,
+            resources.getString(R.string.register_success),
+            Toast.LENGTH_SHORT
+        ).show()
+
+
+        /**
+         * Here the new user registered is automatically signed-in so we just sign-out the user from firebase
+         * and send him to Intro Screen for Sign-In
+         */
+        FirebaseAuth.getInstance().signOut()
+        // Finish the Register Screen
+        finish()
+    }
 }
